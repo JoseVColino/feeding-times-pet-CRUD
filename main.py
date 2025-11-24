4eeb8d7f2c81633e73bb0b5c1a80fa93e17508b5
JoseVColino <josevictorcolino@gmail.com>
2025-11-23 21:20:18 -0300

    feat: main as controller

--- main.py
+++ main.py
@@ -4,33 +4,38 @@ from app.service import PetFeedingService
 
 if __name__ == '__main__':
     init_db()
-    with get_session() as session:
-        repo = PetFeedingRepository(session)
-        service = PetFeedingService(repo)
+    while True:
+        with get_session() as session:
+            repo = PetFeedingRepository(session)
+            service = PetFeedingService(repo)
+            menu = [
+                f'{"":#^30}',
+                f'{"PET FEEDING":-^30}',
+                f'{"":#^30}',
+                '',
+                '1- List Games',
+                '2- Search by ID',
+                '3- Add New Game',
+                '4- Update Game',
+                '5- Delete game'
+                'x- Exit App'
+                ]
+            
+            choices = {
+                '1': service.list_all,
+                '2': service.search_by_id,
+                '3': service.insert,
+                '4': service.update,
+                '5': service.delete_by_id,
+                'x': None 
+            }
 
-        menu = [
-            f'{"PET FEEDING":-^30}',
-            '1- List Games',
-            '2- Search by ID',
-            '3- Add New Game',
-            '4- Update Game',
-            '5- Delete game'
-            'x- Exit App'
-            ]
-        
-        choices = {
-            '1': service.list_all,
-            '2': service.search_by_id,
-            '3': service.insert,
-            '4': service.update,
-            '5': service.delete_by_id,
-            'x': None 
-        }
+            print(*menu,sep='\n')
+            user_input = input('Choose: ')
+            
+            if user_input == 'x':
+                break
 
-        print(*menu,sep='\n')
-        user_input = input()
-        
-        if user_input in choices.keys():
-            res = choices[user_input]()
-            for feeding in res:
-                print(feeding)
+            if user_input in choices.keys():
+                res = choices[user_input]()
+                print(res)
